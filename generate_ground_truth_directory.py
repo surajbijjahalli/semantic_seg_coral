c7c53b4e2d310c3863d9c2d7468421790c64b6a4
surajb <suraj.bijjahalli@gmail.com>
2023-10-09 09:31:22 +1100

    Downsample images and generated masks

--- generate_ground_truth_directory.py
+++ generate_ground_truth_directory.py
@@ -28,13 +28,33 @@ print("Unique semantic labels in this image: ", np.unique(example_label))
 fig,axs = plt.subplots()
 axs.imshow(example_label)
 '''
+
+
+def rescale_image(img,labels,new_width):
+    # Rescale image maintaining aspect ratio and adjust annotation coordinates
+    
+    #Get aspect ratio
+    old_h,old_w = img.shape[:2]
+    aspect_ratio = old_h/old_w
+
+    new_height = aspect_ratio*new_width
+    rescaled_image = cv2.resize(img,(int(new_height),int(new_width)))
+    #old_x = labels['x'].to_numpy()
+    #old_y = labels['y'].to_numpy()
+    #new_x = old_x * new_width/old_w
+    #new_y = old_y * new_height/old_h
+    labels['x'] = labels['x'].multiply(new_width/old_w)
+    labels['y'] = labels['y'].multiply(new_height/old_h)
+    
+    return rescaled_image,labels
+
 #%%
 
 parser = argparse.ArgumentParser("Visualize sparse labels")
 parser.add_argument("--path_img_dir",type=str,help="path to image directory")
 parser.add_argument("--path_annotations",type=str,help="path to sparse annotations")
 parser.add_argument("--path_output",type=str,help="directory to store generated ground truth masks")
-
+parser.add_argument("--path_resized_imgs",type=str,help="directory to store resized images")
 
 args = parser.parse_args()
 # Path to the directory of images
@@ -45,6 +65,7 @@ path_to_annotation_file = args.path_annotations
 
 output_dir = args.path_output
 
+path_to_resized_imgs = args.path_resized_imgs
 
 # Read in the entire annotation file
 label_dataframe = pd.read_csv(path_to_annotation_file)
@@ -64,7 +85,8 @@ pbar = tqdm(list_imgs)
 
 for path_index, path in enumerate(pbar):
     # Pick a random file path from the list
-    sample = random.choice(list_imgs)
+    #sample = random.choice(list_imgs)
+    sample = path
 
     # extract the file name without the extension and convert to an integer - this is necessary since we use the file name as a key for grabbing the sparse annotations
     img_name = int(Path(sample).stem)
@@ -75,8 +97,11 @@ for path_index, path in enumerate(pbar):
 
 
 
-    # Grab the annotations for the sampled image
-    labels = label_dataframe[label_dataframe['quadratid']==img_name]
+    # Grab the annotations for the sampled random image
+    labels = label_dataframe[label_dataframe['quadratid']==img_name].copy()
+
+    
+
 
     # Grab the x-y coordinates of each annotation
     labels_coordinates = labels[['x','y']]
@@ -87,13 +112,19 @@ for path_index, path in enumerate(pbar):
     # Read in the image
     img = plt.imread(path_to_img)
 
+    
+
+
+
+
+    rescaled_img,labels = rescale_image(img,labels,200)
 
     # Create a new dataframe by renaming the columns to be consistent with Fast_mss library
     new_labels = labels.rename(columns={"x": "X", "y": "Y","label": "Label"})
 
 
     # Call fast_mss to generate groung truth from sparse annotations
-    mask = fast_mss(img, new_labels[['X','Y','Label']], unique_labels, 
+    mask = fast_mss(rescaled_img, new_labels[['X','Y','Label']], unique_labels, 
                 start_iter = 2500, end_iter = 20, num_iter = 30, method = 'mode')  
 
     # convert to uint8
@@ -103,3 +134,5 @@ for path_index, path in enumerate(pbar):
 
     # Save mask
     cv2.imwrite(output_dir + '/' +str(img_name)+'_mask.png',mask)
+
+    cv2.imwrite(path_to_resized_imgs + '/' +str(img_name)+'_resized.png',rescaled_img)
--- generate_ground_truth_random_image.py
+++ generate_ground_truth_random_image.py
@@ -42,6 +42,7 @@ path_to_img_dir = args.path_img_dir
 # Path to the sparse annotation csv file
 path_to_annotation_file = args.path_annotations
 
+
 # List of paths for each file in the image directory
 list_imgs = glob.glob(path_to_img_dir+"*.jpg")
 
@@ -68,7 +69,7 @@ dict_keys = list(np.array(range(len(unique_labels))))
 dict_of_labels = dict(zip(dict_keys,unique_labels))
 
 # Grab the annotations for the sampled random image
-labels = label_dataframe[label_dataframe['quadratid']==img_name]
+labels = label_dataframe[label_dataframe['quadratid']==img_name].copy()
 
 # Grab the x-y coordinates of each annotation
 labels_coordinates = labels[['x','y']]
@@ -79,21 +80,44 @@ path_to_img = rand_sample #path_to_img_dir+str(img_name)+".jpg"
 # Read in the image
 img = plt.imread(path_to_img)
 
+def rescale_image(img,labels,new_width):
+    # Rescale image maintaining aspect ratio and adjust annotation coordinates
+    
+    #Get aspect ratio
+    old_h,old_w = img.shape[:2]
+    aspect_ratio = old_h/old_w
+
+    new_height = aspect_ratio*new_width
+    rescaled_image = cv2.resize(img,(int(new_height),int(new_width)))
+    #old_x = labels['x'].to_numpy()
+    #old_y = labels['y'].to_numpy()
+    #new_x = old_x * new_width/old_w
+    #new_y = old_y * new_height/old_h
+    labels['x'] = labels['x'].multiply(new_width/old_w)
+    labels['y'] = labels['y'].multiply(new_height/old_h)
+    
+    return rescaled_image,labels
+
+
+
+rescaled_img,labels = rescale_image(img,labels,200)
+
+
 
 # Create a new dataframe by renaming the columns to be consistent with Fast_mss library
 new_labels = labels.rename(columns={"x": "X", "y": "Y","label": "Label"})
 
 
 # Call fast_mss to generate groung truth from sparse annotations
-mask = fast_mss(img, new_labels[['X','Y','Label']], unique_labels, 
+mask = fast_mss(rescaled_img, new_labels[['X','Y','Label']], unique_labels, 
                 start_iter = 2500, end_iter = 20, num_iter = 30, method = 'mode')  
 
 # convert to uint8
 mask = mask.astype(np.uint8)
 
 # Display the image, sparse annotations, and the generated ground truth mask
-display_overlay(img,colorize_prediction(mask, unique_labels),new_labels)
-
+display_overlay(rescaled_img,colorize_prediction(mask, unique_labels),new_labels)
+'''
 print("Number of images in the dataset: ", len(label_dataframe))
 print("number of unique labels in the entire dataset: ",len(unique_labels))
 print("unique labels: ", unique_labels)
@@ -101,3 +125,5 @@ print("unique labels: ", unique_labels)
 
 # Save mask
 cv2.imwrite(str(img_name)+'_mask.png',mask)
+
+'''
